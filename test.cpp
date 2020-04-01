82288496e7112f6494b7f629dae553007fe5602f
ParkMinKuen <alsrms2011@gmail.com>
2020-04-01 21:31:02 +0900

    test2

--- test.cpp
+++ test.cpp
@@ -7,16 +7,7 @@ int main() {
 	linkedList *L = (linkedList *)malloc(sizeof(linkedList));
 	L->cur = NULL;
 	L->head = NULL;
-	L->tail = NULL;
-
-
-	createNode(L, 1);
-	createNode(L, 2);
-	createNode(L, 3);
-	deleteNode(L);
-	createNode(L, 4);
-	createNode(L, 5);
-	createNode(L, 6);
+	L
 	deleteNode(L);
 	deleteNode(L);
 	createNode(L, 7);
